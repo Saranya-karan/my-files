30af893d6ebad7135ae66077545873bc54a74e52
saranya-karan <saranyakkaran3@gmail.com>
2023-11-20 17:07:03 +0530

    add stringlist

---
+++ stringlist.py
@@ -0,0 +1,21 @@
+class Check:
+    def __init__(self, string):
+        self.string=string
+        
+    def convert(string):
+        listout=[i for i in string1]
+        return listout
+       
+string1 =input("Enter the string  :")
+
+obj=Check(string1)
+print(obj.convert())
+
+
+
+
+
+
+
+
+                
